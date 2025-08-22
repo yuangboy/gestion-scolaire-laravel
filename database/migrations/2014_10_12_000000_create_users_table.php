bd4d599fd2a360dd3ce1ed9c6dd08d96f6a48fc5
Albin_Mp <albinmpobo@gmail.com>
2025-08-22 18:16:24 +0200

    creation table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->enum('role', ['admin', 'student', 'teacher', 'guardian']);
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();  
         });
     }
 
