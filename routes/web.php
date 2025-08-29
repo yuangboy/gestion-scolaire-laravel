f33fecacfedc769d27d9908d71afef3b2f1c20db
ChancelAPOUASSA <chancelapouassa11@gmail.com>
2025-08-29 23:55:53 +0100

    modification des routes

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,10 @@ Route::get("/create-Student",[StudentController::class,'createStudent']);
 
 // Route professeur
 Route::get("/create-Teacher",[TeacherController::class,'createTeacher']);
+
+Route::get("/register",[AuthController::class,'registerView'])->name('register.view');
+Route::post("/register",[AuthController::class,'register'])->name('register');
+
+Route::get("/login",[AuthController::class,'loginView'])->name('login.view');
+Route::post("/login",[AuthController::class,'login'])->name('login');
+
